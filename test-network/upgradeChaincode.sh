c80a3829adc9cc41b7508c1f2b81acd695d9b77a
deanzaka <hdeanzak@id.ibm.com>
2021-09-15 22:46:25 +0700

    add upgrade chaincode script

---
+++ test-network/upgradeChaincode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters. Expected: "
+    echo "./upgradeChaincode.sh <CHAINCODE_VERSION> <CHAINCODE_SEQUENCE>"
+    exit 1 
+fi
+
+./network.sh deployCC -ccn sampleApp -ccp ~/BlockchainDeveloper/SampleApplication/contract -ccl typescript -ccv $1 -ccs $2
