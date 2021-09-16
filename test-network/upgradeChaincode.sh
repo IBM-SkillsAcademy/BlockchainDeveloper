2857fe7d66e9e33912ed2508cc3f7f19c2661651
deanzaka <hdeanzak@id.ibm.com>
2021-09-16 07:09:23 +0700

    fix argument length check on upgrade chaincode

--- test-network/upgradeChaincode.sh
+++ test-network/upgradeChaincode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters. Expected: "
     echo "./upgradeChaincode.sh <CHAINCODE_VERSION> <CHAINCODE_SEQUENCE>"
     exit 1 
