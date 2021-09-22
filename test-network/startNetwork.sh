d00004621e00dc748da82f4c4f0dbbacd019b0de
deanzaka <hdeanzak@id.ibm.com>
2021-09-22 14:52:18 +0200

    delete org3 cert on network down and put it on gitignore

--- test-network/startNetwork.sh
+++ test-network/startNetwork.sh
@@ -4,8 +4,8 @@ docker rm -f $(docker ps -aq); docker volume rm -f $(docker volume ls -q)
 docker network prune
 docker volume prune
 ./network.sh down
- ./network.sh up -s couchdb -ca
- ./network.sh createChannel
+./network.sh up -s couchdb -ca
+./network.sh createChannel
 ./network.sh deployCC -ccn sampleApp -ccp ~/BlockchainDeveloper/SampleApplication/contract -ccl typescript
 
 
