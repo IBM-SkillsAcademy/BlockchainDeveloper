a35d6c7c3e7a39c8161acd09605d52b4e8a59072
deanzaka <hdeanzak@id.ibm.com>
2021-10-13 13:14:17 +0200

    update ex06 commands

--- ex-commands/ex06/startEx06.sh
+++ ex-commands/ex06/startEx06.sh
@@ -12,6 +12,10 @@ ROOT=`pwd`
 # cp ${ROOT}/code-solutions/ex05/collections_config.json ${ROOT}/SampleApplication/contract
 # cp ${ROOT}/code-solutions/ex05/package.json ${ROOT}/SampleApplication/contract
 
+# TODO: temporary copy from ex03, change to ex05 later
+cd ${ROOT}/ex-commands/ex03
+./applySolution.sh
+
 echo "################## START NETWORK ########################"
 cd ${ROOT}/test-network/
 ./startNetwork.sh
@@ -26,16 +30,20 @@ curl -X GET "http://localhost:6001/api/v1/auth/registrar/enroll" -H "accept: */*
 curl -X GET "http://localhost:6002/api/v1/auth/registrar/enroll" -H "accept: */*"; printf "\n";
 curl -X GET "http://localhost:6003/api/v1/auth/registrar/enroll" -H "accept: */*"; printf "\n";
 
-# # create org3 affiliation
-# sleep 3;
-# curl -X GET "http://localhost:6003/api/v1/auth/create-affiliation" -H "accept: */*"; printf "\n";
+# create org3 affiliation
+sleep 3;
+curl -X GET "http://localhost:6003/api/v1/auth/create-affiliation" -H "accept: */*"; printf "\n";
 
-# # register user for cli
-# sleep 3;
-# cd ${ROOT}/ex-commands/ex05/
-# ./enrollUser.sh CLI-Manu-User org1 department1 Manufacturer
-# ./enrollUser.sh CLI-Regu-User org2 department1 Regulator
-# ./enrollUser.sh CLI-Insu-User org3 department1 Insurer
+sleep 3;
+curl -X POST "http://localhost:6001/api/v1/auth/user/register-enroll" -H "accept: */*" -H "Content-Type: application/json" -d "{\"enrollmentID\":\"Manu-User\"}"; printf "\n";
+./populateOrders.sh
+
+# register user for cli
+sleep 3;
+cd ${ROOT}/ex-commands/ex05/
+./enrollUser.sh CLI-Manu-User org1 department1 Manufacturer
+./enrollUser.sh CLI-Regu-User org2 department1 Regulator
+./enrollUser.sh CLI-Insu-User org3 department1 Insurer
 
 # register user for app
 sleep 3;
