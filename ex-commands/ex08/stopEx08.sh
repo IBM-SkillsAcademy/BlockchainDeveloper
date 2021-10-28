9e2bfb79baec3313ccd4fbf819403351ada8affe
Hossam Abdelmoaty <49478584+hossamabdelmoaty@users.noreply.github.com>
2021-10-28 16:02:04 +0200

    edit ex8

--- ex-commands/ex08/stopEx08.sh
+++ ex-commands/ex08/stopEx08.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "################## STOP NETWORK ########################"
-cd ../../Vehicle-Network/
-./byfn.sh down
+cd ../../test-network/
+./stopNetwork.sh
 docker rm -f $(docker ps -aq) 
 docker volume rm -f $(docker volume ls -q)
-
-
-
+echo "stopping the applications " 
+cd /home/blockchain/BlockchainDeveloper/SampleApplication/application
+./stop.sh
 
 
